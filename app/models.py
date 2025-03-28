62f3de7c4132f266968661c82087baf808515169
wanglei318 <15851861998@163.com>
2025-03-28 11:00:30 +0800

    Add app/models.py

---
+++ app/models.py
@@ -0,0 +1,11 @@
+from app import db
+from datetime import datetime
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f'<Task {self.title}>'
